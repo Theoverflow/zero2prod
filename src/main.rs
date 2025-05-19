f3c8e3c2607d6be9cad83bfa09d270d89d6e4ccf
Théo Marini <theomarini@macvie.local>
2025-05-19 13:25:28 +0200

    Add telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 //! src/main.rs
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use z2p::configurations::get_configuration;
 use z2p::startup::run;
+use z2p::telemetry;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = telemetry::get_subscriber("z2p".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber);
+
     let config = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", config.application_port);
--- src/startup.rs
+++ src/startup.rs
@@ -5,6 +5,7 @@ use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub async fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
     // let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
@@ -13,6 +14,7 @@ pub async fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Serve
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection_pool.clone())
